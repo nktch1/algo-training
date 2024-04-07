89e7bad92629d824ec70b89b6f3ab6bc7a882a53
nktch1 <nikitych1@yandex.ru>
2024-04-07 13:12:28 +0300

    Add one more solution

---
+++ 11-Container-With-Most-Water.go
@@ -0,0 +1,22 @@
+package main
+
+func maxArea(height []int) int {
+	left := 0
+	right := len(height) - 1
+	ans := 0
+
+	for left < right {
+		area := min(height[left], height[right]) * (right - left)
+		if area > ans {
+			ans = area
+		}
+
+		if height[left] < height[right] {
+			left++
+		} else {
+			right--
+		}
+	}
+
+	return ans
+}
